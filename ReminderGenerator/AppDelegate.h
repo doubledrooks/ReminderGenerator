c3a2977669ef2e02c9f08922d6889a501a7d5950
Jeff Dean <jeff@zilkey.com>
2013-06-19 21:21:04 -0600

    Initial Commit

---
+++ ReminderGenerator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReminderGenerator
+//
+//  Created by Jeff Dean on 6/19/13.
+//  Copyright (c) 2013 Doubled Rooks Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ReminderGenerator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ReminderGenerator
+//
+//  Created by Jeff Dean on 6/19/13.
+//  Copyright (c) 2013 Doubled Rooks Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
